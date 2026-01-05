07999cc2a2087c7f97f07da6ac8baab24ced7cc5
yusufuca <yusemuca@gmail.com>
2026-01-05 15:26:48 +0300

    building generator

--- Assets/Scripts/ProceduralBuildingGenerator.cs
+++ Assets/Scripts/ProceduralBuildingGenerator.cs
@@ -1,53 +1,148 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ProceduralBuildingGenerator : MonoBehaviour
 {
-    // Variables 
-
     [Header("Prefabs")]
-    public GameObject floor;
-    public GameObject wallSolid;
-    public GameObject wallDiagonal;
-    public GameObject wallWindow;
-    public GameObject wallDoor;
-    public GameObject ceiling;
+    public GameObject floorPrefab;
+    public GameObject ceilingPrefab;
+    public GameObject wallSolidPrefab;
+    public GameObject wallDiagonalPrefab;
+    public GameObject wallWindowPrefab;
+    public GameObject wallDoorPrefab;
 
-    [Header("Settings")]
-    public int buildingSize = 10;
+    public int roomSize = 10;
     public int wallHeight = 4;
-    public int tileSize = 4;
+    public int wallWidth = 4;
+    public int wallDepth = 2;
 
+    [Header("GameSeed")]
     public int gameSeed = 0;
-    private int currentSeed;
+    private int generatedSeed;
+    
+    System.Random rng;
 
     private HashSet<Vector2Int> pickedTiles = new HashSet<Vector2Int>();
-    private System.Random rng;
+    private List<GameObject> spawnedObjects = new List<GameObject>();
 
-
-
-
-    // Generate Seed
-
-    public void resetSeed()
+    private void Start()
     {
-        if(gameSeed == 0)
+        GenerateSeed();
+        GenerateHouse();
+            
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F)) 
         {
-           currentSeed = Random.Range(1, 1000000);
-
+            GenerateHouse();
+           
+        }
+    }
+    void GenerateSeed()
+    {
+        if (gameSeed == 0)
+        {
+         gameSeed = Random.Range(1, 1000000);
+            Debug.Log("Current seed is " + gameSeed);
         }
         else
         {
-            currentSeed = gameSeed;
+            Debug.Log("Current seed is " + gameSeed);
         }
+
+            rng = new System.Random(gameSeed);
     }
-    // Generate House
+
+    void GenerateHouse()
+    {
+        ClearHouse();
+        GenerateTileMap();
+        
+
+    }
+
+    void GenerateTileMap()
+    {
+       //FloorPlan
+
+        Vector2Int currentPos = Vector2Int.zero;
+        pickedTiles.Add(currentPos);
+
+        for (int i = 0; i < roomSize; i++) 
+        {
+            Vector2Int direction = RandomDirection();
+            currentPos += direction;
+            pickedTiles.Add(currentPos);
+        }
+        foreach(Vector2Int tilepos in pickedTiles)
+        {
+            SpawnFloor(tilepos);
+            CheckAndSpawn(tilepos);
+            
+        }
+
+    }
+
+    private Vector2Int RandomDirection()
+    {
+        int pickTile = rng.Next(0, 4);
+
+        if (pickTile == 0) return Vector2Int.up;
+        if (pickTile == 1) return Vector2Int.down;
+        if (pickTile == 2) return Vector2Int.left;
+        else return Vector2Int.right;
+                
+    }
+
+    void CheckAndSpawn(Vector2Int pos)
+    {
+        int ws = wallWidth / 2;
+        
+        bool left = pickedTiles.Contains(pos + Vector2Int.left);
+        bool right = pickedTiles.Contains(pos + Vector2Int.right);
+        bool up = pickedTiles.Contains(pos+Vector2Int.up);
+        bool down = pickedTiles.Contains(pos+Vector2Int.down);
 
 
-    // Drunk Walker House Shape Creation
+        if (!left) { SpawnWalls(pos, 270, ws * Vector3.left); }
+        if (!right) { SpawnWalls(pos, 90, ws * Vector3.right); }
+        if (!up) { SpawnWalls(pos, 0, ws*Vector3.forward); }
+        if(!down) { SpawnWalls(pos,180, ws* Vector3.back); }
 
-    // Check and Spawn Walls 
 
-    // Spawner Methods 
+    }
+
+
+
+    private void SpawnWalls(Vector2Int tilepos, float rot,Vector3 offset)
+    {
+        Vector3 worldPos = new Vector3(tilepos.x * wallWidth, 0, tilepos.y * wallHeight);
+        Vector3 finalPos = worldPos + offset;
+        Quaternion rotation = Quaternion.Euler(0, rot, 0);
+        GameObject obj = Instantiate(wallSolidPrefab, finalPos, rotation, transform);
+        spawnedObjects.Add(obj);
+    }
+
+    private void SpawnFloor(Vector2Int tilepos)
+    {
+        Vector3 worldPos = new Vector3(tilepos.x * wallWidth, 0, tilepos.y * wallHeight);
+        GameObject obj = Instantiate(floorPrefab,worldPos, Quaternion.identity, transform);
+
+        spawnedObjects.Add(obj);
+    }
+
+    void ClearHouse()
+    {
+        // Destroy all the old game objects
+        foreach (GameObject obj in spawnedObjects)
+        {
+            if (obj != null) Destroy(obj);
+        }
+        spawnedObjects.Clear();
+        pickedTiles.Clear();
+    }
+
 }
