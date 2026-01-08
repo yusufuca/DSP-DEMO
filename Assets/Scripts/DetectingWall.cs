15de167c7119d5f2890f09a82bed3acb363f37bf
yusufuca <yusemuca@gmail.com>
2026-01-08 21:41:59 +0300

    update detecting wall

--- Assets/Scripts/DetectingWall.cs
+++ Assets/Scripts/DetectingWall.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DetectingWall : MonoBehaviour
 {
@@ -12,6 +13,16 @@ public class DetectingWall : MonoBehaviour
     public string windowWall = "wallWindow";
     public string doorWall = "wallDoor";
     public float maxDistance = 20f;
+    
+    
+    [Header("Text")]
+    public TextMeshProUGUI frontText;
+    public TextMeshProUGUI backText;
+    public TextMeshProUGUI leftText;
+    public TextMeshProUGUI rightText;
+
+  
+
 
     private void Update()
     {
@@ -22,36 +33,144 @@ public class DetectingWall : MonoBehaviour
     {
        Vector3 origin = transform.position + (Vector3.up);
 
+        //   Front
 
-
-        if (Physics.Raycast(origin, transform.forward, out RaycastHit hit, maxDistance))
+        if (Physics.Raycast(origin, transform.forward, out RaycastHit hitFront, maxDistance))
         {
-           float distance = hit.distance;
-            if (hit.collider.CompareTag(solidWall))
+           float distance = hitFront.distance;
+            frontText.text = "Distance: " + distance;
+            
+            if (hitFront.collider.CompareTag(solidWall))
             {
-                Debug.Log("SolidWall Detected. Distance =" + distance);
-                Debug.DrawLine(origin, hit.point, Color.green);
+                Debug.Log("SolidWall Detected front. Distance =" + distance);
+                Debug.DrawLine(origin, hitFront.point, Color.green);
             }
-            else if (hit.collider.CompareTag(diagonalWall))
+            else if (hitFront.collider.CompareTag(diagonalWall))
             {
-                Debug.Log("DiagonalWall Detected. Distance =" + distance);
-                Debug.DrawLine(origin, hit.point, Color.green);
+                Debug.Log("DiagonalWall Detected front. Distance =" + distance);
+                Debug.DrawLine(origin, hitFront.point, Color.green);
             }
-            else if (hit.collider.CompareTag(windowWall))
+            else if (hitFront.collider.CompareTag(windowWall))
             {
-                Debug.Log("WindowWall Detected. Distance =" + distance);
-                Debug.DrawLine(origin, hit.point, Color.green);
+                Debug.Log("WindowWall Detected front. Distance =" + distance);
+                Debug.DrawLine(origin, hitFront.point, Color.green);
             }
-            else if (hit.collider.CompareTag(doorWall))
+            else if (hitFront.collider.CompareTag(doorWall))
             {
-                Debug.Log("DoorWall Detected. Distance =" + distance);
-                Debug.DrawLine(origin, hit.point, Color.green);
+                Debug.Log("DoorWall Detected front. Distance =" + distance);
+                Debug.DrawLine(origin, hitFront.point, Color.green);
             }
 
         }
-        else { Debug.Log("Nothing");
-            Debug.DrawLine(origin, hit.point, Color.green);
+        else { Debug.Log("Nothing front");
+            Debug.DrawLine(origin, hitFront.point, Color.green);
         }
+        
+        // Back
+
+
+        if (Physics.Raycast(origin, -transform.forward, out RaycastHit hitBack, maxDistance))
+        {
+            float distance = hitBack.distance;
+            backText.text = "Distance: " + distance;
+
+            if (hitBack.collider.CompareTag(solidWall))
+            {
+                Debug.Log("SolidWall Detected back. Distance =" + distance);
+                Debug.DrawLine(origin, hitBack.point, Color.red);
+            }
+            else if (hitBack.collider.CompareTag(diagonalWall))
+            {
+                Debug.Log("DiagonalWall Detected back. Distance =" + distance);
+                Debug.DrawLine(origin, hitBack.point, Color.red);
+            }
+            else if (hitBack.collider.CompareTag(windowWall))
+            {
+                Debug.Log("WindowWall Detected back. Distance =" + distance);
+                Debug.DrawLine(origin, hitBack.point, Color.red);
+            }
+            else if (hitBack.collider.CompareTag(doorWall))
+            {
+                Debug.Log("DoorWall Detected back. Distance =" + distance);
+                Debug.DrawLine(origin, hitBack.point, Color.red);
+            }
+
+        }
+        else
+        {
+            Debug.Log("Nothing back");
+            Debug.DrawLine(origin, hitBack.point, Color.red);
+        }
+
+        // Right
+
+
+        if (Physics.Raycast(origin, transform.right, out RaycastHit hitRight, maxDistance))
+        {
+            float distance = hitRight.distance;
+            rightText.text = "Distance: " + distance;
+            if (hitRight.collider.CompareTag(solidWall))
+            {
+                Debug.Log("SolidWall Detected right. Distance =" + distance);
+                Debug.DrawLine(origin, hitRight.point, Color.blue);
+            }
+            else if (hitRight.collider.CompareTag(diagonalWall))
+            {
+                Debug.Log("DiagonalWall Detected right. Distance =" + distance);
+                Debug.DrawLine(origin, hitRight.point, Color.blue);
+            }
+            else if (hitRight.collider.CompareTag(windowWall))
+            {
+                Debug.Log("WindowWall Detected right. Distance =" + distance);
+                Debug.DrawLine(origin, hitRight.point, Color.blue);
+            }
+            else if (hitRight.collider.CompareTag(doorWall))
+            {
+                Debug.Log("DoorWall Detected right. Distance =" + distance);
+                Debug.DrawLine(origin, hitRight.point, Color.blue);
+            }
+
+        }
+        else
+        {
+            Debug.Log("Nothing right");
+            Debug.DrawLine(origin, hitRight.point, Color.blue);
+        }
+
+        //  Left
+
+        if (Physics.Raycast(origin, -transform.right, out RaycastHit hitLeft, maxDistance))
+        {
+            float distance = hitLeft.distance;
+            leftText.text = "Distance: " + distance;
+            if (hitLeft.collider.CompareTag(solidWall))
+            {
+                Debug.Log("SolidWall Detected Left. Distance =" + distance);
+                Debug.DrawLine(origin, hitLeft.point, Color.magenta);
+            }
+            else if (hitLeft.collider.CompareTag(diagonalWall))
+            {
+                Debug.Log("DiagonalWall Detected Left. Distance =" + distance);
+                Debug.DrawLine(origin, hitLeft.point, Color.magenta);
+            }
+            else if (hitLeft.collider.CompareTag(windowWall))
+            {
+                Debug.Log("WindowWall Detected Left. Distance =" + distance);
+                Debug.DrawLine(origin, hitLeft.point, Color.magenta);
+            }
+            else if (hitLeft.collider.CompareTag(doorWall))
+            {
+                Debug.Log("DoorWall Detected Left. Distance =" + distance);
+                Debug.DrawLine(origin, hitLeft.point, Color.magenta);
+            }
+
+        }
+        else
+        {
+            Debug.Log("Nothing Left");
+            Debug.DrawLine(origin, hitLeft.point, Color.magenta);
+        }
+
     }
 
 }
