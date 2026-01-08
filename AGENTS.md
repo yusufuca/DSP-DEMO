d6eef31b91a602ef3814c5cf4ced94b8887ee423
yusufuca <yusemuca@gmail.com>
2026-01-08 22:12:49 +0300

    write debug parameters screen

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: DSP-DEMO
 - Unity version: Unity 2022.3.62f1
 - Active game object:
-  - Name: RoadManager
-  - Tag: Untagged
-  - Layer: Default
+  - Name: PlayerCapsule
+  - Tag: Player
+  - Layer: 8
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/DetectingWall.cs
+++ Assets/Scripts/DetectingWall.cs
@@ -38,31 +38,38 @@ public class DetectingWall : MonoBehaviour
         if (Physics.Raycast(origin, transform.forward, out RaycastHit hitFront, maxDistance))
         {
            float distance = hitFront.distance;
-            frontText.text = "Distance: " + distance;
+            
             
             if (hitFront.collider.CompareTag(solidWall))
             {
+                frontText.text = "Front SolidWall Detected Distance: " + distance;
                 Debug.Log("SolidWall Detected front. Distance =" + distance);
                 Debug.DrawLine(origin, hitFront.point, Color.green);
             }
             else if (hitFront.collider.CompareTag(diagonalWall))
             {
+                frontText.text = "Front DiagonalWall Detected Distance: " + distance;
                 Debug.Log("DiagonalWall Detected front. Distance =" + distance);
                 Debug.DrawLine(origin, hitFront.point, Color.green);
             }
             else if (hitFront.collider.CompareTag(windowWall))
             {
+                frontText.text = "Front Window Detected Distance: " + distance;
                 Debug.Log("WindowWall Detected front. Distance =" + distance);
                 Debug.DrawLine(origin, hitFront.point, Color.green);
             }
             else if (hitFront.collider.CompareTag(doorWall))
             {
+                frontText.text = "Front Door Detected Distance: " + distance;
                 Debug.Log("DoorWall Detected front. Distance =" + distance);
                 Debug.DrawLine(origin, hitFront.point, Color.green);
             }
 
         }
-        else { Debug.Log("Nothing front");
+        else 
+        {
+            frontText.text = "Front Distance: Null";
+            Debug.Log("Nothing front");
             Debug.DrawLine(origin, hitFront.point, Color.green);
         }
         
@@ -72,25 +79,29 @@ public class DetectingWall : MonoBehaviour
         if (Physics.Raycast(origin, -transform.forward, out RaycastHit hitBack, maxDistance))
         {
             float distance = hitBack.distance;
-            backText.text = "Distance: " + distance;
+            
 
             if (hitBack.collider.CompareTag(solidWall))
             {
+                backText.text = "Back SolidWall Detected Distance: " + distance;
                 Debug.Log("SolidWall Detected back. Distance =" + distance);
                 Debug.DrawLine(origin, hitBack.point, Color.red);
             }
             else if (hitBack.collider.CompareTag(diagonalWall))
             {
+                backText.text = "Back Diagonal Detected Distance: " + distance;
                 Debug.Log("DiagonalWall Detected back. Distance =" + distance);
                 Debug.DrawLine(origin, hitBack.point, Color.red);
             }
             else if (hitBack.collider.CompareTag(windowWall))
             {
+                backText.text = "Back Window Detected Distance: " + distance;
                 Debug.Log("WindowWall Detected back. Distance =" + distance);
                 Debug.DrawLine(origin, hitBack.point, Color.red);
             }
             else if (hitBack.collider.CompareTag(doorWall))
             {
+                backText.text = "Back Door Detected Distance: " + distance;
                 Debug.Log("DoorWall Detected back. Distance =" + distance);
                 Debug.DrawLine(origin, hitBack.point, Color.red);
             }
@@ -98,6 +109,7 @@ public class DetectingWall : MonoBehaviour
         }
         else
         {
+            backText.text = "Back Distance: Null";
             Debug.Log("Nothing back");
             Debug.DrawLine(origin, hitBack.point, Color.red);
         }
@@ -108,24 +120,28 @@ public class DetectingWall : MonoBehaviour
         if (Physics.Raycast(origin, transform.right, out RaycastHit hitRight, maxDistance))
         {
             float distance = hitRight.distance;
-            rightText.text = "Distance: " + distance;
+            
             if (hitRight.collider.CompareTag(solidWall))
             {
+                rightText.text = "Right SolidWall Detected Distance: " + distance;
                 Debug.Log("SolidWall Detected right. Distance =" + distance);
                 Debug.DrawLine(origin, hitRight.point, Color.blue);
             }
             else if (hitRight.collider.CompareTag(diagonalWall))
             {
+                rightText.text = "Right DiagonalWall Detected Distance: " + distance;
                 Debug.Log("DiagonalWall Detected right. Distance =" + distance);
                 Debug.DrawLine(origin, hitRight.point, Color.blue);
             }
             else if (hitRight.collider.CompareTag(windowWall))
             {
+                rightText.text = "Right Window Detected Distance: " + distance;
                 Debug.Log("WindowWall Detected right. Distance =" + distance);
                 Debug.DrawLine(origin, hitRight.point, Color.blue);
             }
             else if (hitRight.collider.CompareTag(doorWall))
             {
+                rightText.text = "Right Door Detected Distance: " + distance;
                 Debug.Log("DoorWall Detected right. Distance =" + distance);
                 Debug.DrawLine(origin, hitRight.point, Color.blue);
             }
@@ -133,6 +149,7 @@ public class DetectingWall : MonoBehaviour
         }
         else
         {
+            rightText.text = "Right Distance: Null";
             Debug.Log("Nothing right");
             Debug.DrawLine(origin, hitRight.point, Color.blue);
         }
@@ -142,24 +159,28 @@ public class DetectingWall : MonoBehaviour
         if (Physics.Raycast(origin, -transform.right, out RaycastHit hitLeft, maxDistance))
         {
             float distance = hitLeft.distance;
-            leftText.text = "Distance: " + distance;
+            
             if (hitLeft.collider.CompareTag(solidWall))
             {
+                leftText.text = "Left SolidWall Detected Distance: " + distance;
                 Debug.Log("SolidWall Detected Left. Distance =" + distance);
                 Debug.DrawLine(origin, hitLeft.point, Color.magenta);
             }
             else if (hitLeft.collider.CompareTag(diagonalWall))
             {
+                leftText.text = "Left DiagonalWall Detected Distance: " + distance;
                 Debug.Log("DiagonalWall Detected Left. Distance =" + distance);
                 Debug.DrawLine(origin, hitLeft.point, Color.magenta);
             }
             else if (hitLeft.collider.CompareTag(windowWall))
             {
+                leftText.text = "Left Window Detected Distance: " + distance;
                 Debug.Log("WindowWall Detected Left. Distance =" + distance);
                 Debug.DrawLine(origin, hitLeft.point, Color.magenta);
             }
             else if (hitLeft.collider.CompareTag(doorWall))
             {
+                leftText.text = "Left Door Detected Distance: " + distance;
                 Debug.Log("DoorWall Detected Left. Distance =" + distance);
                 Debug.DrawLine(origin, hitLeft.point, Color.magenta);
             }
@@ -168,6 +189,7 @@ public class DetectingWall : MonoBehaviour
         else
         {
             Debug.Log("Nothing Left");
+            leftText.text = "Left Distance: Null";
             Debug.DrawLine(origin, hitLeft.point, Color.magenta);
         }
 
